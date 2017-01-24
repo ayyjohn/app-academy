aaf65daf8dcbe50ca8b5b0c4d855a97e1e7ce543
Alec Johnson <alecjohnson@Alecs-MacBook-Pro.local>
2017-01-23 21:33:21 -0800

    commented enumerables.rb

--- w1d1/enumerables.rb
+++ w1d1/enumerables.rb
@@ -1,8 +1,12 @@
 class Array
 
   def my_each(&prc)
+    # copy the Array#each method
+    # returns the original array, but applies the given
+    # block to each element
 
     i=0
+
     while i < self.length
       prc.call(self[i])
       i += 1
@@ -10,7 +14,12 @@ class Array
   end
 
   def my_select(&prc)
+    # copy the Array#select method
+    # returns a new array with only the elements for which the
+    # given block returned true
+
     keep = []
+
     self.my_each do |el|
       keep << el if prc.call(el)
     end
@@ -18,7 +27,12 @@ class Array
   end
 
   def my_reject(&prc)
+    # copy the Array#reject method
+    # returns a new array with only the elemnts for which the given
+    # block returned false
+
     keep = []
+
     self.my_each do |el|
       keep << el if !prc.call(el)
     end
@@ -26,6 +40,10 @@ class Array
   end
 
   def my_any(&prc)
+    # copy the Array#any method
+    # returns true if any of the elements in the array returned
+    # true from the block given
+
     self.my_each do |el|
       return true if prc.call(el)
     end
@@ -33,11 +51,15 @@ class Array
   end
 
   def my_flatten(flattened_array = [])
+    # copy the Array#flatten method
+    # takes an array of arrays and recursively returns
+    # an array with all of the elements in one array
+
     self.inject(flattened_array) do |array, item|
-      p array, item
       if item.is_a?(Integer)
         flattened_array << item
       else
+        # if the item isn't a int, it's an array which needs to be flat
         item.my_flatten(flattened_array)
       end
     end
@@ -45,7 +67,12 @@ class Array
   end
 
   def my_rotate(num_times = 1)
+    # rotates elements from front to back or back to front in a
+    # one dimensional array. If positive, elements move
+    # from right to left, if negative elements move from right to left.
+
     self_duped = self.dup
+
     if num_times > 0
       num_times.times do |el|
         self_duped << self_duped.shift
@@ -57,7 +84,13 @@ class Array
   end
 
   def my_zip(*args)
+    # takes multiple arrays and converts them into an array
+    # of arrays where all the elements in each array at the same
+    # position are in the same array
+    # eg [1,2,3].zip([4,5,6][7,8,9]) #=> [[1,4,7],[2,5,8],[3,6,9]]
+
     zipped = Array.new (self.length) { [] }
+
     self.length.times do |i|
       zipped[i] << self[i]
       args.each do |arg|
@@ -69,7 +102,11 @@ class Array
 
 
   def my_join(delimiter = "")
+    # takes an array and returns a string of the elements joined by the
+    # delimiter value
+
     string = ""
+
     self.my_each do |char|
       string += char + delimiter
     end
@@ -81,10 +118,13 @@ class Array
   end
 
   def my_reverse
+    # takes an array and returns the array with the elements in
+    # reverse order.
+    
     dupe = self.dup
     half = dupe.length / 2
     dupe[0..half].each_with_index do |element, index|
-      dupe[index], dupe[-1-index] = dupe[-1-index], dupe[index] 
+      dupe[index], dupe[-1-index] = dupe[-1-index], dupe[index]
     end
     dupe
   end
