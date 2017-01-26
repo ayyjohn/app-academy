c7b97aaa7c92b7397c1d6a4bc3222c4ef9ab042c
Alec Johnson <alecjohnson@Alecs-MacBook-Pro.local>
2017-01-25 18:58:56 -0800

    added one liners and commented

--- w1/d3/recursion/deep_dup.rb
+++ w1/d3/recursion/deep_dup.rb
@@ -1,9 +1,26 @@
 class Array
   def deep_dup(new_arr = [])
+    # return a duplicate array where the references to each
+    # array in the array are also different; therefore
+    # for any level of depth, no matter whether or not the
+    # contents are mutable, there won't be a link between
+    # the original array and the duped array
 
     self.each do |el|
       return new_arr << el.dup unless el.is_a?(Array)
       new_arr << el.deep_dup
     end
+  end
+
+  def deep_dup_inj
+    # one line deep dup with inject
+    
+    inject([]) { |dup, el| dup << (el.is_a?(Array) ? el.deep_dup_inj : el.dup) }
+  end
+
+  def deep_dup_map
+    # one line deep dup with map
+
+    map { |el| el.is_a?(Array) ? el.deep_dup_map : el.dup }
   end
 end
