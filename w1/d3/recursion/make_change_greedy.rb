1f13b934a097049acf1c5cca2c32fdc2b38ac70b
Alec Johnson <alecjohnson@Alecs-MacBook-Pro.local>
2017-01-25 19:55:45 -0800

    added comments

--- w1/d3/recursion/make_change_greedy.rb
+++ w1/d3/recursion/make_change_greedy.rb
@@ -1,4 +1,8 @@
 def make_change_greedy(amount, coins)
+  # returns an array of coins needed to make change for the amount.
+  # calculates this value greedily, ie takes the largest coin
+  # possible at each attempt; this guarantees a solution
+  # but does not guarantee the least amount of coins given back
 
   return [] if amount == 0
   coin_arr = []
