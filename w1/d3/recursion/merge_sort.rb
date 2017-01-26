ff661e9da62e22d1c6e06743e8a2babca059baab
Alec Johnson <alecjohnson@Alecs-MacBook-Pro.local>
2017-01-25 19:53:11 -0800

    switched slice to drop/take for readability, commented code

--- w1/d3/recursion/merge_sort.rb
+++ w1/d3/recursion/merge_sort.rb
@@ -1,13 +1,22 @@
 def merge_sort(array)
-
+  # returns a sorted array by implementing the merge sort
+  # algorithm recursively.
+  
   return array if array.length <= 1
   middle = array.length / 2
-  left = array.slice(0...middle)
-  right = array.slice(middle..-1)
+  left = array.take(middle)
+  right = array.drop(middle)
   merge(merge_sort(left), merge_sort(right))
 end
 
 def merge(array1, array2)
+  # helper method for merge_sort
+  # returns two arrays merged in sorted order
+  # by iterating through both until one is empty, adding the
+  # first element of whichever array starts with a smaller element
+
+  # optimization: if one array is empty, add the rest of the array
+  # to the merged array in one function call
   merged = []
   until array1.empty? || array2.empty?
     if array1.first < array2.first
